3ba211a305f7ec54084712e87ad0dfa005aa6904
virak0001 <53550608+virak0001@users.noreply.github.com>
2019-09-07 10:59:10 +0700

    Update async_await.js

--- js/async_await.js
+++ js/async_await.js
@@ -1,19 +1,19 @@
 const url = 'https://gorest.co.in/public-api/users?_format=json&access-token=nK23wA4W9JGqBZBVizoMEyLxhwyY16rzDTKT';
-// async function userID() {
-// const table = document.querySelector('table');
-// const response = await fetch(url);
-// const data = await response.json();
-//     data.result.forEach(element => {
-//         const {id, first_name, last_name,gender} = element;
-//         table.innerHTML +=
-//         `
-//             <tr>
-//                 <td>${id}</td>
-//                 <td>${first_name}</td>
-//                 <td>${last_name}</td>
-//                 <td>${gender}</td>
-//             </tr>
-//         `
-//     });
-// }
-// userID();
+async function userID() {
+const table = document.querySelector('table');
+const response = await fetch(url);
+const data = await response.json();
+    data.result.forEach(element => {
+        const {id, first_name, last_name,gender} = element;
+        table.innerHTML +=
+        `
+            <tr>
+                <td>${id}</td>
+                <td>${first_name}</td>
+                <td>${last_name}</td>
+                <td>${gender}</td>
+            </tr>
+        `
+    });
+}
+userID();
